b2286e149da578570bd2570de4929636751292a6
Reece Rose <reecerose18@gmail.com>
2021-12-01 18:31:28 -0500

    Created solana project and started work on it.

---
+++ solana/tests/solana.js
@@ -0,0 +1,58 @@
+const anchor = require("@project-serum/anchor");
+
+const { SystemProgram } = anchor.web3;
+
+describe("solana", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const initializeBaseAccount = async (program, baseAccount) => {
+    return await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+  };
+
+  it("Gets initialized account", async () => {
+    const program = anchor.workspace.Solana;
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const tx = await initializeBaseAccount(program, baseAccount);
+
+    console.log("Your transaction signature", tx);
+
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("GIF Count", account.totalGifs.toString());
+    // Assert gifCount to be 0
+  });
+
+  it("Can update gif count", async () => {
+    const program = anchor.workspace.Solana;
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const tx = await initializeBaseAccount(program, baseAccount);
+
+    console.log("Your transaction signature", tx);
+
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    // assert 0
+    console.log("GIF Count", account.totalGifs.toString());
+    await program.rpc.addGif({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    // assert 1
+    console.log("GIF Count", account.totalGifs.toString());
+  });
+});
