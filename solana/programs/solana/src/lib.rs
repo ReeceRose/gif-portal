39e36c5cf7832c9d9e4f9db94b4d83c0b9491fa0
Reece Rose <reecerose18@gmail.com>
2021-12-01 18:59:13 -0500

    Got actual program id and deployed to devnet

--- solana/programs/solana/src/lib.rs
+++ solana/programs/solana/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4mkC1jawAiSYRcmuhzpJdehMXmYkUwv5qctW2r7W6ttM");
 
 #[program]
 pub mod solana {
